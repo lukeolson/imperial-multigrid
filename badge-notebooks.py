e8c9a53da7d60371989e54f5f4b6164030489c95
Luke Olson <luke.olson@gmail.com>
2020-05-05 17:17:21 -0500

    remove badge. add readme file

--- badge-notebooks.py
+++ badge-notebooks.py
@@ -3,6 +3,11 @@ import json
 import sys
 import os
 
+remove = False
+if len(sys.argv) == 2:
+    if sys.argv[1] == 'remove':
+        remove = True
+
 fnames = glob.glob('*.ipynb')
 
 for fname in fnames:
@@ -11,7 +16,8 @@ for fname in fnames:
     with open(fname, "rt", encoding="utf-8") as inf:
         d = json.load(inf)
 
-    s = fr'[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/lukeolson/imperial-multigrid/blob/master/{dirname}/{fname})'
+    s = fr'<a href="https://colab.research.google.com/github/lukeolson/imperial-multigrid/blob/master/{dirname}/{fname}" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>'
+    #s = fr'[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/lukeolson/imperial-multigrid/blob/master/{dirname}/{fname})'
     newcell = {}
     newcell['cell_type'] = "markdown"
     newcell['metadata'] = {"colab_type": "text",
@@ -25,6 +31,8 @@ for fname in fnames:
             doit = True
         else:
             print(f'{fname}: already badged')
+            if remove:
+                d['cells'].pop(0)
     else:
         doit = True
 
